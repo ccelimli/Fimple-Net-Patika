000a7bda7b270a246c7d21984c1f47fa31495a03
Çağatay Çelimli <c.celimli@live.co.uk>
2023-11-23 01:50:33 +0300

    Variables

---
+++ ConsoleProgramming/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("İsminizi Giriniz: ");
+string Name=Console.ReadLine();
+Console.WriteLine("Soyisminizi Giriniz: ");
+string Surname=Console.ReadLine();
+Console.WriteLine("Merhaba "+Name+" "+ Surname);
