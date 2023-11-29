727e57e5e109bf0ebb3ed65a8bc770f7ec27138a
Çağatay Çelimli <c.celimli@live.co.uk>
2023-11-29 14:28:56 +0300

    Struct

---
+++ Struct/Program.cs
@@ -0,0 +1,40 @@
+﻿Dikdortgen dikdortgen = new Dikdortgen();
+Console.WriteLine("Class: "+ dikdortgen.Hesapla().ToString());
+
+Dikdortgen_Struct dikdortgen_Struct;
+dikdortgen_Struct.Uzun = 5;
+dikdortgen_Struct.Kisa = 5;
+
+Console.WriteLine("Struct: "+ dikdortgen_Struct.Hesapla().ToString());
+class Dikdortgen
+{
+    public int Uzun;
+    public int Kisa;
+
+    public Dikdortgen()
+    {
+        Uzun = 3;
+        Kisa = 1;
+    }
+
+    public int Hesapla()
+    {
+        return Uzun * Kisa;
+    }
+}
+
+struct Dikdortgen_Struct
+{
+    public int Uzun;
+    public int Kisa;
+
+    public Dikdortgen_Struct(int Uzun, int Kisa)
+    {
+        this.Uzun = Uzun; this.Kisa = Kisa;
+    }
+
+    public int Hesapla()
+    {
+        return Uzun * Kisa;
+    }
+}
