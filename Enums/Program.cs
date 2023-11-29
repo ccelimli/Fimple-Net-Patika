db542a946f7522c652e6005c1a5fe3648a935e1d
Çağatay Çelimli <c.celimli@live.co.uk>
2023-11-29 20:02:39 +0300

    Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.WriteLine(Days.Pazartesi);
+Console.WriteLine((int)Days.Pazar);
+
+int sicaklik = 32;
+
+if (sicaklik <= (int)HavaDurumu.Normal)
+{
+    Console.WriteLine("Havanın biraz daha ısınmasını bekleyebilirsiniz.");
+}
+else if (sicaklik>= (int)HavaDurumu.Sicak)
+{
+    Console.WriteLine("D1şarıya çıkmak için çok sıcak bir gün");
+}
+else if (sicaklik>=(int)HavaDurumu.Normal && sicaklik<(int)HavaDurumu.CokSicak)
+{
+    Console.WriteLine("Hadi dışarıya çıkalım!");
+}
+
+
+enum Days
+{
+    Pazartesi,
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma,
+    Cumartesi=22,
+    Pazar,
+}
+
+enum HavaDurumu
+{
+    Soguk=5,
+    Normal=20,
+    Sicak=25,
+    CokSicak=30
+}
