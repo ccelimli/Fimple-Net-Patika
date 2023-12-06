b72f5df03b41235b9127f918377bf590dfa3e0fb
Çağatay Çelimli <c.celimli@live.co.uk>
2023-12-06 22:48:50 +0300

    Phone Book

--- ConsoleProgramming/Program.cs
+++ ConsoleProgramming/Program.cs
@@ -1,5 +1,1 @@
-﻿Console.WriteLine("İsminizi Giriniz: ");
-string Name=Console.ReadLine();
-Console.WriteLine("Soyisminizi Giriniz: ");
-string Surname=Console.ReadLine();
-Console.WriteLine("Merhaba "+Name+" "+ Surname);
+﻿
